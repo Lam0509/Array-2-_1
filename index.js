c8ca9063330d4eeb8459ca2734b88fe33e018c60
Lam0509 <112493001+Lam0509@users.noreply.github.com>
2022-09-13 22:40:32 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+// Bài 1
+// let myColor = ["Red", "Green", "White", "Black"];
+// myColor.join();
+// document.write(m
+
+// Bài 2
+// let num = prompt('Nhập 1 số bất kỳ');
+// let str = Array.from(num);
+// let result = [str[0]];
+// for (let x = 1; x < str.length; x++)
+// {
+//     if ((parseInt(str[x - 1]) % 2 === 0) && (parseInt(str[x]) % 2 === 0))
+//     {
+//         result.push(' - ', str[x]);
+//     }
+//     else
+//     {
+//         result.push(str[x]);
+//     }
+// }
+// document.write(result.join(''));
+
+// Bài 3
+// let str = Array.from(window.prompt());
+// let str2 = [];
+// for (let i = 0; i < str.length; i++) {
+//     if (str[i] === str[i].toLowerCase()) {
+//         str[i] = str[i].toUpperCase();
+//         str2.push(str[i]);
+//     } else if (str[i] === str[i].toUpperCase()) {
+//         str[i] = str[i].toLowerCase();
+//         str2.push(str[i]);
+//     }
+// }
+// document.write(str2.join(''));
+
